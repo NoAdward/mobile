955b88e53ad32c6f093b1cae67015f1e2b81ef8f
NoAdward <dima.tomchuk.99@gmail.com>
2023-04-09 23:47:59 +0300

    basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { Routes, RouterModule } from '@angular/router';
 import { NotFoundError } from 'rxjs';
 
-const appRoutes: Routes= [
-  { path:'', component: HomeComponent},
-  { path: 'about', component: AboutComponent},
-  { path: '**', component: NotFoundComponent}
-]
+
+
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'profile', loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule) }
+];
+
+// const appRoutes: Routes= [
+//   { path:'', component: HomeComponent},
+//   { path: 'about', component: AboutComponent},
+//   { path: '**', component: NotFoundComponent}
+// ]
 
 @NgModule({
   declarations: [
@@ -22,7 +30,8 @@ const appRoutes: Routes= [
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
